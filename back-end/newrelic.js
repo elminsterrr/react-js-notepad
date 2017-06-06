11da8ec1fba34a3c6d21830c4269ef36bf9f9338
elminsterrr <elminsterthewhite@gmail.com>
2017-06-06 19:56:18 +0200

    make it better

---
+++ back-end/newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Note Rest API'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '533e8b20d51bc2ea99d31fe084adebc146f8bcba',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
