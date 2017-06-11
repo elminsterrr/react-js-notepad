fe95ab3c0daa5e76278b0e73bd7f3fcf0d28c58b
elminsterrr <elminsterthewhite@gmail.com>
2017-06-11 23:29:37 +0200

    Add autoprefixer

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'sass-loader']
+          use: ['css-loader','postcss-loader', 'sass-loader']
         })
       }
     ]
